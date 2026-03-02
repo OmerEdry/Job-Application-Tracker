c8331b25832bae747602c369946eff4613539b38
Sharon Anderman <sharon.ande3@gmail.com>
2026-03-02 16:19:29 +0200

    feat: update JobCard to use sx parameter instead of sxOverrides

--- src/components/kanban/JobCard.jsx
+++ src/components/kanban/JobCard.jsx
@@ -3,7 +3,7 @@ import BaseKanbanCard from "./BaseKanbanCard";
 const JobCard = ({ job }) => {
     return (
         <BaseKanbanCard
-            sxOverrides={(theme) => ({
+            sx={(theme) => ({
                 background: theme.palette[job.status].gradient,
                 border: 0,
             })}
