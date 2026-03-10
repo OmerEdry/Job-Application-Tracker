5428b5d916a9a3afaa2b4e3b5da39cddaf690926
Omer Edry <Edry2omer@gmail.com>
2026-03-10 16:50:16 +0200

    feat: implement OfferEmptyState in KanbanColumn

--- src/components/JobApplications/kanban/KanbanColumn.jsx
+++ src/components/JobApplications/kanban/KanbanColumn.jsx
@@ -2,17 +2,21 @@ import { Box } from '@mui/material';
 import KanbanHeaderCard from './KanbanHeaderCard';
 import KanbanJobStack from './KanbanJobStack';
 import EmptyJobCard from './EmptyJobCard';
-
+import OfferEmptyState from './OfferEmptyState'; // הקומפוננטה החדשה
 
 const KanbanColumn = ({ header, jobCards }) => {
+    const isOfferColumn = header.name.toLowerCase() === 'offer';
+
     return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2,ht: '100%' }}>
-        <KanbanHeaderCard header={header} />
-        {jobCards.length > 0 ? 
-        (<KanbanJobStack>{jobCards}</KanbanJobStack>) :
-        (<EmptyJobCard status={header.name.toLowerCase()} icon={header.icon} />)}
-    </Box>
-)
-}
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, height: '100%' }}>
+            <KanbanHeaderCard header={header} />
+            
+            {jobCards.length > 0 ? (
+                <KanbanJobStack>{jobCards}</KanbanJobStack>) : 
+                (isOfferColumn ? <OfferEmptyState /> : <EmptyJobCard status={header.name.toLowerCase()} icon={header.icon} />
+            )}
+        </Box>
+    );
+};
 
 export default KanbanColumn;
