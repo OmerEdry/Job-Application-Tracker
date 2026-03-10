f4008b21986573a08909e1bb284fe4f5058e52be
Omer Edry <Edry2omer@gmail.com>
2026-03-10 16:35:17 +0200

    feat: implement EmptyJobCard in KanbanColumn

--- src/components/JobApplications/kanban/KanbanColumn.jsx
+++ src/components/JobApplications/kanban/KanbanColumn.jsx
@@ -1,17 +1,18 @@
 import { Box } from '@mui/material';
 import KanbanHeaderCard from './KanbanHeaderCard';
 import KanbanJobStack from './KanbanJobStack';
+import EmptyJobCard from './EmptyJobCard';
 
 
 const KanbanColumn = ({ header, jobCards }) => {
     return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-            <KanbanHeaderCard header={header} />
-            <KanbanJobStack>
-                {jobCards}
-            </KanbanJobStack>
-        </Box>
-    )
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2,ht: '100%' }}>
+        <KanbanHeaderCard header={header} />
+        {jobCards.length > 0 ? 
+        (<KanbanJobStack>{jobCards}</KanbanJobStack>) :
+        (<EmptyJobCard status={header.name.toLowerCase()} icon={header.icon} />)}
+    </Box>
+)
 }
 
 export default KanbanColumn;
