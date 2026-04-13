1fd0be555e5443866b6f02a08887e5757034a55c
Sharon Anderman <sharon.ande3@gmail.com>
2026-04-14 01:08:11 +0300

    refactor:  Added all fields controlled by the user, restructred how the fields are being exported

---
+++ src/components/JobApplications/jobsConfig.js
@@ -0,0 +1,30 @@
+import { uniqueJobs, uniqueSkills } from "../../mockJobs/mockJobs";
+
+
+const COMPANY_NAME = { name: 'companyName', label: 'Company Name', placeholder: 'Select company', type: 'autocomplete', freeSolo: true, options: uniqueJobs, required: true };
+const COMPANY_LOGO = { name: 'companyLogo', label: 'Company Logo', type: 'text', required: false };
+const JOB_TITLE = { name: 'jobTitle', label: 'Job Title', type: 'autocomplete', freeSolo: true, required: true };
+const LOCATION = { name: 'location', label: 'Location', type: 'text', required: false };
+const WORK_TYPE = { name: 'workType', label: 'Work Type', type: 'autocomplete', freeSolo: true, required: true };
+const STATUS = { name: 'status', label: 'Status', type: 'autocomplete', required: true, options: ['Wishlist', 'Applied', 'Interviewing', 'Offer', 'Archive'] };
+const JOB_URL = { name: 'jobUrl', label: 'Job URL', type: 'text', required: false };
+const TAGS = { name: 'tags', label: 'Tags', placeholder: 'Max 5', type: 'autocomplete', multiple: true, freeSolo: true, limitTags: 2, options: uniqueSkills, };
+const APPLIED_DATE = { name: 'appliedDate', label: 'Applied Date', type: 'date', required: true };
+const PLATFORM = { name: 'platform', label: 'Platform', type: 'text', };
+const NEXT_INTERVIEW_DATE = { name: 'nextInterviewDate', label: 'Interview Date', type: 'date', required: true };
+const ROUND = { name: 'round', label: 'Round', type: 'text', };
+const ANSWER_DEADLINE = { name: 'answerDeadline', label: 'Answer Deadline', type: 'date' };
+const OFFER_AMOUNT = { name: 'offerAmount', label: 'Offer Amount', type: 'text' };
+
+
+export const STATUSES = ['Wishlist', 'Applied', 'Interviewing', 'Offer'];
+
+export const STATUS_FIELDS_CONFIG = {
+    Wishlist: [
+        COMPANY_NAME, COMPANY_LOGO, JOB_TITLE, LOCATION,
+        WORK_TYPE, STATUS, JOB_URL, TAGS
+    ],
+    Applied: [APPLIED_DATE, PLATFORM],
+    Interviewing: [NEXT_INTERVIEW_DATE, ROUND],
+    Offer: [ANSWER_DEADLINE, OFFER_AMOUNT]
+};
