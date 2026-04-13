2766dc5395957dd53ee8e190be97d40ddb37e2a9
Sharon Anderman <sharon.ande3@gmail.com>
2026-04-14 01:00:08 +0300

    refactor: Moved the outer label of the inputs from baseForm.jsx into input.jsx and now the label is part of the input itself.

--- src/components/controls/Input.jsx
+++ src/components/controls/Input.jsx
@@ -1,4 +1,4 @@
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete, TextField, Box, FormLabel, Typography } from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers";
 
 
@@ -8,93 +8,118 @@ export default function InputField({ field, value, onChange, disabled }) {
     const { name, label, placeholder, type, required, options, multiple, freeSolo, limitTags } = field;
 
 
+
     switch (type) {
         case 'autocomplete':
             return (
-                <Autocomplete
-                    multiple={multiple}
-                    freeSolo={freeSolo}
-                    limitTags={limitTags}
-                    size='small'
-                    options={options || []}
-                    value={value !== undefined ? value : (multiple ? [] : null)}
-                    onChange={(event, newValue) => onChange(name, newValue)}
-                    disabled={disabled}
+                <Box>
+                    <FormLabel required={field.required} sx={{ display: 'block', color: 'text.primary' }}>
+                        <Typography variant='input'>
+                            {field.label}
+                        </Typography>
+                    </FormLabel>
 
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            label={placeholder}
-                            required={required}
-                            size='small'
-                            fullWidth
-                            sx={{
-                                '& .MuiFormLabel-asterisk': {
-                                    display: 'none',
-                                },
-                            }}
-                            slotProps={{
-                                inputLabel: {
-                                    sx: {
-                                        '&:not(.MuiInputLabel-shrink)': {
-                                            transform: 'translate(14px, 7px) scale(1)',
+                    <Autocomplete
+                        multiple={multiple}
+                        freeSolo={freeSolo}
+                        limitTags={limitTags}
+                        size='small'
+                        options={options || []}
+                        value={value !== undefined ? value : (multiple ? [] : null)}
+                        onChange={(event, newValue) => onChange(name, newValue)}
+                        disabled={disabled}
+
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label={placeholder}
+                                required={required}
+                                size='small'
+                                fullWidth
+                                sx={{
+                                    '& .MuiFormLabel-asterisk': {
+                                        display: 'none',
+                                    },
+                                }}
+                                slotProps={{
+                                    inputLabel: {
+                                        sx: {
+                                            '&:not(.MuiInputLabel-shrink)': {
+                                                transform: 'translate(14px, 7px) scale(1)',
+                                            }
                                         }
-                                    }
-                                },
-                            }}
-                        />
-                    )}
-                    slotProps={{
-                        popper: {
-                            placement: 'bottom-start',
-                            modifiers: [{ name: 'flip', enabled: false }]
-                        },
-                        inputLabel: { required: false },
-                    }}
-                />
+                                    },
+                                }}
+                            />
+                        )}
+                        slotProps={{
+                            popper: {
+                                placement: 'bottom-start',
+                                modifiers: [{ name: 'flip', enabled: false }]
+                            },
+                            inputLabel: { required: false },
+                        }}
+                    />
+                </Box>
             );
 
         case 'date':
             return (
-                <DatePicker
-                    label={label}
-                    value={value || null}
-                    onChange={(newValue) => onChange(name, newValue)}
-                    disabled={disabled}
-                    slotProps={{
-                        textField: {
-                            fullWidth: true,
-                            size: 'small',
-                            required: required
-                        },
-                    }}
-                    sx={{
-                        '& .MuiFormLabel-asterisk': {
-                            display: 'none',
-                        },
-                    }}
-                />
+                <Box>
+                    <FormLabel required={field.required} sx={{ display: 'block', color: 'text.primary' }}>
+                        <Typography variant='input'>
+                            {field.label}
+                        </Typography>
+                    </FormLabel>
+
+                    <DatePicker
+                        label={label}
+                        value={value || null}
+                        onChange={(newValue) => onChange(name, newValue)}
+                        disabled={disabled}
+                        slotProps={{
+                            textField: {
+                                fullWidth: true,
+                                size: 'small',
+                                required: required
+                            },
+                        }}
+                        sx={{
+                            '& .MuiFormLabel-asterisk': {
+                                display: 'none',
+                            },
+                        }}
+                    />
+                </Box>
             );
 
         case 'text':
             return (
-                <TextField
-                    fullWidth
-                    size="small"
-                    label={placeholder}
-                    name={name}
-                    value={value || ''}
-                    onChange={(event) => onChange(name, event.target.value)}
-                    disabled={disabled}
-                    required={required}
-                    variant="outlined"
+                <Box>
+                    <FormLabel required={field.required} sx={{ display: 'block', color: 'text.primary' }}>
+                        <Typography variant='input'>
+                            {field.label}
+                        </Typography>
+                    </FormLabel>
 
-                    sx={{
-                        '& .MuiFormLabel-asterisk': {
-                            display: 'none',
-                        },
-                    }}
-                />
+                    <TextField
+                        fullWidth
+                        size="small"
+                        label={placeholder}
+                        name={name}
+                        value={value || ''}
+                        onChange={(event) => onChange(name, event.target.value)}
+                        disabled={disabled}
+                        required={required}
+                        variant="outlined"
+
+                        sx={{
+                            '& .MuiFormLabel-asterisk': {
+                                display: 'none',
+                            },
+                        }}
+                    />
+                </Box>
             );
     };
 };
