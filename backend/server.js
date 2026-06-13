3d2a4d64335f8b36f0db2029f05d7e18ab12a04e
Omer Edry <Edry2omer@gmail.com>
2026-06-13 23:31:18 +0300

    feat(backend): restrict CORS access to allow frontend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,21 @@ const app = express();
 const PORT = 5000;
 
 
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:5173', //defult Vite port
+    'http://localhost:3000'  //backup port
+];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'CORS Policy: Access denied for this origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
 app.use(express.json());
 
 //MOUNT ROUTES
